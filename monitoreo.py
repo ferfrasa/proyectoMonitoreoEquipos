379c3960a4216faad0495a1aeb5a9433a2756693
Erika F <erfefrasa@hotmail.com>
2020-02-09 20:37:25 -0500

    Caractersticas por protocolo SNMP parte 2

--- monitoreo.py
+++ monitoreo.py
@@ -5,3 +5,48 @@ Created on Sun Feb  9 20:26:49 2020
 @author: user
 """
 
+#Monitoreo con protocolo SNMP
+
+
+#Importar la libreria SNMP
+from pysnmp.hlapi import *
+
+
+ipEquipo='192.168.0.3'
+puerto=161
+
+
+if __name__ == "__main__":
+    #funcion para leer las caracteristicas del equipo a monitorear
+    caracteristicas= getCmd( SnmpEngine(),
+                             CommunityData('public'),
+                             UdpTransportTarget((ipEquipo,puerto)),
+                             ContextData(),
+                             ObjectType(ObjectIdentity('SNMPv2-MIB','sysDescr',0)))
+    #Extraer objetos de la lista
+    informacion= next(caracteristicas)
+    print(informacion)
+    
+def informacionSistemaOperativo(OID):
+        e1, e2, e3, vb =next( getCmd( SnmpEngine(),
+                             CommunityData('public', mpModel=0),
+                             UdpTransportTarget((ipEquipo,puerto)),
+                             ContextData(),
+                             ObjectType(ObjectIdentity(OID)))
+    )
+   #Imprimir errores de indicacion
+    if(e1):
+        print("1. "+e1+"\n")
+  #Imprimir errores de estado
+    if(e2):
+        print("2. "+e2+"\n")      
+  #Imprimir errores de estado
+    if(e3):
+        print("3. "+e3+"\n")  
+   #Imprimir errores de dispositivo
+    if(vb):
+        for i in vb:
+            print(str(i)+"\n")     
+            
+            
+def     
